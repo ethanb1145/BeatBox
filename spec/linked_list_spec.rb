c0c83a7bc35b2e29cc073b63ef58774349ab02a5
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-07 16:38:52 -0600

    Add Class and initialize to pass first spec test

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -1,1 +1,16 @@
+require "rspec"
 require "./lib/linked_list"
+require "./lib/node"
+
+
+describe LinkedList do
+    describe "#initialize" do
+        it "exists" do 
+            list = LinkedList.new
+
+            expect(list).to be_an_instance_of(LinkedList)
+        end
+    end
+
+
+end
