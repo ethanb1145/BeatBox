26acfc6cecc20a3616b2d42e2789cb746574d250
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-07 14:49:36 -0600

    Add first working test

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,26 +1,31 @@
+require "rspec"
 require "./lib/node"
 
+
 describe Node do
-    it "exists" do 
-        node1 = Node.new("plop")
+    describe "#initialize" do
+        it "exists" do 
+            node1 = Node.new("plop")
 
-        expect(node1).to be_an_instance_of(Node)
-    end
-
-    it "can print the data inside of the node" do 
-        node1 = Node.new("plop")
-
-        expect(node.data).to eq("plop") 
-    end
-
-    it "can move us to the next node" do 
-        node1 = Node.new("plop")
-        node2 = Node.new("pah")
-
-        expect(node.next_node).to eq 
+            expect(node1).to be_an_instance_of(Node)
+        end
     end
 
 
+    xdescribe "#data" do
+        it "can print the data inside of the node" do 
+            node1 = Node.new("plop")
 
+            expect(node.data).to eq("plop") 
+        end
+    end
 
+    xdescribe "next_node" do 
+        it "can move us to the nil node" do 
+            node1 = Node.new("plop")
+            node2 = Node.new("pah")
+
+            expect(node.next_node).to be(nil)
+        end
+    end
 end
