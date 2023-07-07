59edb74a89cd5ebc57568d983a29265e4be265ef
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-07 13:30:17 -0600

    Add 'node.data' and 'node.next_node' tests, save current progress

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -7,6 +7,20 @@ describe Node do
         expect(node1).to be_an_instance_of(Node)
     end
 
+    it "can print the data inside of the node" do 
+        node1 = Node.new("plop")
+
+        expect(node.data).to eq("plop") 
+    end
+
+    it "can move us to the next node" do 
+        node1 = Node.new("plop")
+        node2 = Node.new("pah")
+
+        expect(node.next_node).to eq 
+    end
+
+
 
 
 end
