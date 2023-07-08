25fed902a08d2bde9bb69ed8077339ff6f61c0e3
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-08 12:55:22 -0600

    Add working append method and tail method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,20 +1,40 @@
 require "./lib/node"
 
 class LinkedList
-    attr_accessor :head
+    attr_accessor :head, :data
 
     def initialize
         @head = nil
-
     end
 
-    def head
-        @head = nil
-    end
+    # def head
+    #     @head = nil
+    # end
 
     def append(data)
-        
+        if @head
+            tail.next_node = Node.new(data)
+        else
+            @head = Node.new(data)
+
+        end
+
+    end
+
+    def tail
+        node = @head
+
+        return node if !node.next_node
+        return node if !node.next_node while (node = node.next_node)
+
     end
 
 
 end
+
+list = LinkedList.new
+list.head
+list.append(10)
+p list
+list.append(20)
+p list
